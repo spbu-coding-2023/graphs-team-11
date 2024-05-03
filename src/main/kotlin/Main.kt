fd2f574611923d4a4d2dcfc605be87e8a89128bb
VersusXX <sultanovmmt@gmail.com>
2024-05-03 20:45:25 +0300

    refactor: algorithms menu
    Refactored code for Algorithms menu.
    Renamed App name.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,10 @@
-import androidx.compose.animation.*
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
-import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import components.algoritms_view.AlgoritmListComponent
+import components.algoritms_view.AlgorithmMenu
 import components.graph_view.GrahpView
 import components.graph_view.GrahpViewComponent
 import data.tools.graphGenerators.flowerSnark
@@ -34,9 +19,7 @@ fun App() {
 
     MaterialTheme {
         Row{
-//            Card(modifier = Modifier.fillMaxHeight()) {
-            DraggableMenuContent()
-//            }
+            AlgorithmMenu()
 
             Card {
                 GrahpViewComponent(gv, showNodes = true)
@@ -47,41 +30,7 @@ fun App() {
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(onCloseRequest = ::exitApplication, title = "BDSM Graphs") {
         App()
     }
-
-}
-
-
-@Composable
-fun DraggableMenuContent() {
-    val menuWidth = 200.dp
-    var menuVisible by remember { mutableStateOf(true) }
-    val density = LocalDensity.current
-
-    Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxHeight().background(Color.White)) {
-
-        AnimatedVisibility(visible = menuVisible, enter = slideInHorizontally {
-            with(density) { -40.dp.roundToPx() }
-        } + expandHorizontally(
-            expandFrom = Alignment.Start
-        ) + fadeIn(
-            initialAlpha = 0.3f
-        ), exit = slideOutHorizontally() + shrinkHorizontally() + fadeOut()) {
-            Surface(
-                modifier = Modifier.width(menuWidth).fillMaxHeight(),
-                shape = RoundedCornerShape(topEnd = 12.dp, bottomEnd = 12.dp)
-            ) {
-                AlgoritmListComponent()
-            }
-        }
-        Icon(imageVector = if (menuVisible) Icons.AutoMirrored.Filled.KeyboardArrowLeft
-        else Icons.AutoMirrored.Filled.KeyboardArrowRight,
-            contentDescription = "Menu",
-            modifier = Modifier.size(30.dp).clickable {
-                menuVisible = !menuVisible
-            })
-    }
-
 }
---
+++ src/main/kotlin/components/algoritms_view/AlgoritmsMenu.kt
@@ -0,0 +1,79 @@
+package components.algoritms_view
+
+import androidx.compose.animation.*
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
+import kotlin.reflect.full.createInstance
+
+@Composable
+@Preview
+fun AlgorithmMenu() {
+    val menuWidth = 200.dp
+    var menuVisible by remember { mutableStateOf(true) }
+    val density = LocalDensity.current
+
+    Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxHeight().background(Color.White)) {
+
+        AnimatedVisibility(visible = menuVisible, enter = slideInHorizontally {
+            with(density) { -40.dp.roundToPx() }
+        } + expandHorizontally(
+            expandFrom = Alignment.Start
+        ) + fadeIn(
+            initialAlpha = 0.3f
+        ), exit = slideOutHorizontally() + shrinkHorizontally() + fadeOut()) {
+            Surface(
+                modifier = Modifier.width(menuWidth).fillMaxHeight(),
+                shape = RoundedCornerShape(topEnd = 12.dp, bottomEnd = 12.dp)
+            ) {
+                AlgoritmList()
+            }
+        }
+        Icon(imageVector = if (menuVisible) Icons.AutoMirrored.Filled.KeyboardArrowLeft
+        else Icons.AutoMirrored.Filled.KeyboardArrowRight,
+            contentDescription = "Menu",
+            modifier = Modifier.size(30.dp).clickable {
+                menuVisible = !menuVisible
+            })
+    }
+}
+
+@Composable
+@Stable
+fun AlgoritmList() {
+
+    val algo = AlgoritmFinder()
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .background(Color.LightGray).padding(8.dp).verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Top,
+    ) {
+        Text(text = "Algorithms")
+        Divider(color = Color.Black, modifier = Modifier.fillMaxWidth(0.3f))
+        for (i in algo.algoritms) {
+            Text(text = i.simpleName.toString(), modifier = Modifier.clickable() {
+                // i.members.forEach { println(it.name)}
+                val runAlgo = i.members.single { it.name == "alogRun" }
+                val algoExpample = i.createInstance()
+
+                runAlgo.call(algoExpample)
+            })
+
+        }
+    }
+}

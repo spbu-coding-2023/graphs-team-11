defc7a9e246ccc33e63063613e6ed9efd6f62e2a
VersusXX <sultanovmmt@gmail.com>
2024-05-05 18:03:37 +0300

    add: community detection using Leiden alg
    Added initial version of community detection using Leiden algorithm.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,22 @@
+import algorithm.main.detectCommunities
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import ui.algoritms_view.AlgorithmMenu
 import ui.graph_view.GrahpView
 import ui.graph_view.GrahpViewComponent
-import data.tools.graphGenerators.flowerSnark
 import data.tools.graphGenerators.randomTree
-import data.tools.graphGenerators.starUndirected
-import data.algoritms.*
 
 @Composable
 @Preview
 fun App() {
 
     val g = randomTree(10)
+
+    detectCommunities(g)
 
     val gv = GrahpView(g)
 
--- src/main/kotlin/algorithm/main/CommunityDetection.kt
+++ src/main/kotlin/algorithm/main/CommunityDetection.kt
@@ -1,4 +1,98 @@
 package algorithm.main
 
-class CommunityDetection {
+import data.Graph
+
+fun <D> detectCommunities(graph: Graph<D>) {
+    val leiden = LeidenAlgorithm(graph)
+    val communities = leiden.detectCommunities()
+
+    // print community index stating with 1 and nodes that belong to that community
+    var communityIndex = 1
+    val communityMap = mutableMapOf<Int, MutableList<D>>()
+    communities.forEach { (node, community) ->
+        if (communityMap.containsKey(community)) {
+            communityMap[community]!!.add(node)
+        } else {
+            communityMap[community] = mutableListOf(node)
+        }
+    }
+    communityMap.forEach { (_, nodes) ->
+        println("Community $communityIndex: $nodes")
+        ++communityIndex
+    }
+
 }
+
+class LeidenAlgorithm<D>(private val graph: Graph<D>) {
+    private var communities = mutableMapOf<D, Int>()
+    private var reversedGraph: Graph<D>
+
+    init {
+        initializeCommunities()
+        reversedGraph = graph.reverse()
+    }
+
+    private fun initializeCommunities() {
+        graph.vertices.keys.forEach { node ->
+            communities[node] = graph.vertices.keys.indexOf(node)
+        }
+    }
+
+    fun detectCommunities(): Map<D, Int> {
+        var improvement = true
+        while (improvement) {
+            improvement = localMovingAlgorithm()
+            refinePartition()
+        }
+        return communities
+    }
+
+    private fun localMovingAlgorithm(): Boolean {
+        var improved = false
+        graph.vertices.keys.shuffled().forEach { node ->
+            val bestCommunity = findBestCommunity(node)
+            if (bestCommunity != communities[node]) {
+                communities[node] = bestCommunity
+                improved = true
+            }
+        }
+        return improved
+    }
+
+    private fun findBestCommunity(node: D): Int {
+        val neighborCommunities = mutableSetOf<Int>()
+
+        // Add communities of outgoing neighbors
+        graph.vertices[node]?.forEach { (neighbor, _) ->
+            neighborCommunities.add(communities[neighbor]!!)
+            graph.vertices[neighbor]?.forEach { (inNeighbor, _) ->
+                neighborCommunities.add(communities[inNeighbor]!!)
+            }
+        }
+
+
+        // Add communities of incoming neighbors
+        reversedGraph.vertices[node]?.forEach { (inNeighbor, _) ->
+            neighborCommunities.add(communities[inNeighbor]!!)
+        }
+
+        return neighborCommunities.maxByOrNull { community ->
+            val communityConnections = (graph.vertices[node] ?: emptyList()).count { edge ->
+                communities[edge.first] == community || communities.containsKey(edge.first) && communities[edge.first] == community
+            }
+            communityConnections
+        } ?: communities[node]!!
+    }
+
+
+
+    private fun refinePartition() {
+        graph.vertices.keys.forEach { node ->
+            val bestCommunity = findBestCommunity(node)
+            if (bestCommunity != communities[node]) {
+                communities[node] = bestCommunity
+            }
+        }
+    }
+}
+

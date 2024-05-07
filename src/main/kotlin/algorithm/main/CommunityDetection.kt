469454799ab588d73207533ddeba76f4409c9e42
VersusXX <sultanovmmt@gmail.com>
2024-05-07 12:32:34 +0300

    refactor: community-detection structure
    removed unnecessary function and unnecessary prints

--- src/main/kotlin/algorithm/main/CommunityDetection.kt
+++ src/main/kotlin/algorithm/main/CommunityDetection.kt
@@ -2,31 +2,9 @@ package algorithm.main
 
 import androidx.compose.ui.graphics.Color
 import data.Graph
-import kotlinx.coroutines.processNextEventInCurrentThread
 import ui.graph_view.graph_view_actions.NodeViewUpdate
 import ui.graph_view.graph_view_actions.Update
 import kotlin.random.Random
-
-fun <D> detectCommunities(graph: Graph<D>) {
-    val leiden = LeidenAlgorithm(graph)
-    val communities = leiden.detectCommunities()
-
-    // print community index stating with 1 and nodes that belong to that community
-    var communityIndex = 1
-    val communityMap = mutableMapOf<Int, MutableList<D>>()
-    communities.forEach { (node, community) ->
-        if (communityMap.containsKey(community)) {
-            communityMap[community]!!.add(node)
-        } else {
-            communityMap[community] = mutableListOf(node)
-        }
-    }
-    communityMap.forEach { (_, nodes) ->
-        println("Community $communityIndex: $nodes")
-        ++communityIndex
-    }
-
-}
 
 class LeidenToRun: Algoritm() {
     override fun <D> alogRun(graph: Graph<D>): Update<D> {
@@ -42,12 +20,12 @@ class LeidenToRun: Algoritm() {
                 communityMap[community] = mutableListOf(node)
             }
         }
-        communityMap.forEach { (_, nodes) ->
-            println("Community $communityIndex: $nodes")
-            ++communityIndex
-        }
+//        communityMap.forEach { (_, nodes) ->
+//            println("Community $communityIndex: $nodes")
+//            ++communityIndex
+//        }
 
-        var colors: MutableMap<Int, Color> = mutableMapOf()
+        val colors: MutableMap<Int, Color> = mutableMapOf()
         for ((community, _) in communityMap) { colors[community] = Color(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))}
 
 
@@ -56,7 +34,7 @@ class LeidenToRun: Algoritm() {
             update[node] = NodeViewUpdate(color = colors[community])
         }
 
-        return Update<D>(nodeViewUpdate = update)
+        return Update(nodeViewUpdate = update)
     }
 }
 
@@ -106,13 +84,13 @@ class LeidenAlgorithm<D>(private val graph: Graph<D>) {
                 neighborCommunities.add(communities[inNeighbor]!!)
             }
         }
-
-
+        
         // Add communities of incoming neighbors
-        reversedGraph.vertices[node]?.forEach { (inNeighbor, _) ->
-            neighborCommunities.add(communities[inNeighbor]!!)
+        if (graph.vertices[node]?.size == 0) {
+            reversedGraph.vertices[node]?.forEach { (inNeighbor, _) ->
+                neighborCommunities.add(communities[inNeighbor]!!)
+            }
         }
-
         return neighborCommunities.maxByOrNull { community ->
             val communityConnections = (graph.vertices[node] ?: emptyList()).count { edge ->
                 communities[edge.first] == community || communities.containsKey(edge.first) && communities[edge.first] == community
@@ -120,8 +98,6 @@ class LeidenAlgorithm<D>(private val graph: Graph<D>) {
             communityConnections
         } ?: communities[node]!!
     }
-
-
 
     private fun refinePartition() {
         graph.vertices.keys.forEach { node ->

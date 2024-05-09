2fc8e1dd2868d56e12f11d35014844bb19b84960
AlexandrKudrya <alexandr.kudrya@gmail.com>
2024-05-10 05:55:47 +0700

    refactor: algorithm's class comeback II

--- src/main/kotlin/model/algoritms/SampleAlgo.kt
+++ src/main/kotlin/model/algoritms/SampleAlgo.kt
@@ -6,19 +6,21 @@ import model.graph_model.graph_model_actions.NodeViewUpdate
 import model.graph_model.graph_model_actions.Update
 import model.graph_model.graph_model_actions.VertViewUpdate
 
-fun <D> sampleAlgo(graph: Graph<D>): Update<D> {
-    val updateNode: MutableMap<D, NodeViewUpdate<D>> = mutableMapOf()
-    val updateVert: MutableMap<D, MutableMap<D, VertViewUpdate<D>>> = mutableMapOf()
-    for (i in graph.vertices) {
-        if (i.value.size % 2 == 1) {
-            updateNode[i.key] = NodeViewUpdate(color = Color.Red, radius = i.value.size.toFloat() * 5 + 5)
-        } else {
-            updateNode[i.key] = NodeViewUpdate(color = Color.Green, radius = i.value.size.toFloat() * 5 + 5)
+class SampleAlgo : Algoritm {
+    override fun <D> alogRun(graph: Graph<D>): Update<D> {
+        val updateNode: MutableMap<D, NodeViewUpdate<D>> = mutableMapOf()
+        val updateVert: MutableMap<D, MutableMap<D, VertViewUpdate<D>>> = mutableMapOf()
+        for (i in graph.vertices) {
+            if (i.value.size % 2 == 1) {
+                updateNode[i.key] = NodeViewUpdate(color = Color.Red, radius = i.value.size.toFloat() * 5 + 5)
+            } else {
+                updateNode[i.key] = NodeViewUpdate(color = Color.Green, radius = i.value.size.toFloat() * 5 + 5)
+            }
+            updateVert[i.key] = mutableMapOf()
+            for (j in i.value) {
+                updateVert[i.key]!!.set(j.first, VertViewUpdate<D>(color = Color.Yellow))
+            }
         }
-        updateVert[i.key] = mutableMapOf()
-        for (j in i.value) {
-            updateVert[i.key]!!.set(j.first, VertViewUpdate<D>(color = Color.Yellow))
-        }
+        return Update<D>(nodeViewUpdate = updateNode, vertViewUpdate = updateVert)
     }
-    return Update<D>(nodeViewUpdate = updateNode, vertViewUpdate = updateVert)
 }
--- src/main/kotlin/model/algoritms/SomeThingLikeDFS.kt
+++ src/main/kotlin/model/algoritms/SomeThingLikeDFS.kt
@@ -5,29 +5,31 @@ import model.graph_model.Graph
 import model.graph_model.graph_model_actions.Update
 import model.graph_model.graph_model_actions.VertViewUpdate
 
-fun <D> SomethingLikeDFS(graph: Graph<D>): Update<D> {
-    val vertUpdate: MutableMap<D, MutableMap<D, VertViewUpdate<D>>> = mutableMapOf()
-    val visited: MutableSet<D> = mutableSetOf()
-    for ((i, verts) in graph.vertices) {
-        vertUpdate[i] = mutableMapOf()
-        for ((j, _) in verts) {
-            vertUpdate[i]!![j] = VertViewUpdate(alpha = 0.5f, color = Color.White)
-        }
-    }
-
-    fun DFS(parent: D) {
-        for ((node, _) in graph.vertices.getOrDefault(parent, mutableSetOf())) {
-            if (node !in visited) {
-                vertUpdate[parent]!![node] = VertViewUpdate(color = Color.Red)
-                visited.add(node)
-                DFS(node)
+class SomeThingLikeDFS: Algoritm {
+    override fun <D> alogRun(graph: Graph<D>): Update<D> {
+        var vertUpdate: MutableMap<D, MutableMap<D, VertViewUpdate<D>>> = mutableMapOf()
+        var visited: MutableSet<D> = mutableSetOf()
+        for ((i, verts) in graph.vertices) {
+            vertUpdate[i] = mutableMapOf()
+            for ((j, _) in verts) {
+                vertUpdate[i]!!.set(j, VertViewUpdate(alpha = 0.5f, color = Color.White))
             }
         }
-    }
 
-    for ((i, _) in graph.vertices) {
-        if (i !in visited) DFS(i)
-    }
+        fun DFS(parent: D) {
+            for ((node, _) in graph.vertices.getOrDefault(parent, mutableSetOf())) {
+                if (node !in visited) {
+                    vertUpdate[parent]!!.set(node, VertViewUpdate(color = Color.Red))
+                    visited.add(node)
+                    DFS(node)
+                }
+            }
+        }
 
-    return Update(vertViewUpdate = vertUpdate)
+        for ((i, _) in graph.vertices) {
+            if (i !in visited) DFS(i)
+        }
+
+        return Update<D>(vertViewUpdate = vertUpdate)
+    }
 }

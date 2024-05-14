5391bd3a44e7e5cfe0dac6b77e4b4983f22cd7f1
VersusXX <sultanovmmt@gmail.com>
2024-05-15 02:35:27 +0300

    refactor: generators to work with Int, String, Float, Char

--- src/main/kotlin/data/tools/graphGenerators/randomTree.kt
+++ src/main/kotlin/data/tools/graphGenerators/randomTree.kt
@@ -1,9 +1,19 @@
 package data.tools.graphGenerators
 
 import model.graph_model.Graph
+import ui.components.generateStringNodeNames
+import viewmodel.IntroWindowVM
 import kotlin.random.Random
 
-fun randomTree(n: Int): Graph<Int> {
+fun randomTree(n: Int, type: IntroWindowVM.GraphKeyType): Graph<*> = when (type) {
+    IntroWindowVM.GraphKeyType.INT -> randomTreeInt(n)
+    IntroWindowVM.GraphKeyType.STRING -> randomTreeString(n)
+    IntroWindowVM.GraphKeyType.FLOAT -> randomTreeFloat(n)
+    IntroWindowVM.GraphKeyType.CHAR -> randomTreeChar(n)
+}
+
+
+private fun randomTreeInt(n: Int): Graph<Int> {
     val graph = Graph<Int>()
 
     graph.addNode(0)
@@ -13,6 +23,59 @@ fun randomTree(n: Int): Graph<Int> {
     for (i in 2..n) {
         graph.addNode(i)
         graph.addVertice(Random.nextInt(0, i - 1), i)
+    }
+
+    return graph
+}
+
+private fun randomTreeString(n: Int): Graph<String> {
+    val graph = Graph<String>()
+    val nodeNames = generateStringNodeNames(n)
+
+    graph.addNode(nodeNames[0])
+    graph.addNode(nodeNames[1])
+    graph.addVertice(nodeNames[0], nodeNames[1])
+
+    for (i in 2..<n) {
+        graph.addNode(nodeNames[i])
+
+    }
+
+    for (i in 2..<n) {
+        graph.addVertice(nodeNames[Random.nextInt(0, i - 1)], nodeNames[i])
+    }
+
+    return graph
+}
+
+private fun randomTreeFloat(n: Int): Graph<Float> {
+    val graph = Graph<Float>()
+
+    graph.addNode(0f)
+    graph.addNode(1f)
+    graph.addVertice(0f, 1f)
+
+    for (i in 2..n) {
+        graph.addNode(i.toFloat())
+        graph.addVertice(Random.nextFloat() * (i - 1), i.toFloat())
+    }
+
+    return graph
+}
+
+private fun randomTreeChar(n: Int): Graph<Char> {
+    val graph = Graph<Char>()
+
+    graph.addNode('A')
+    graph.addNode('B')
+    graph.addVertice('A', 'B')
+
+    for (i in 2..<n) {
+        graph.addNode('A' + i)
+    }
+
+    for (i in 2..<n) {
+        graph.addVertice('A' + Random.nextInt(0, i - 1), 'A' + i)
     }
 
     return graph
--- src/main/kotlin/data/tools/graphGenerators/star.kt
+++ src/main/kotlin/data/tools/graphGenerators/star.kt
@@ -1,29 +1,105 @@
 package data.tools.graphGenerators
 
 import model.graph_model.Graph
+import ui.components.generateStringNodeNames
+import viewmodel.IntroWindowVM
 
-fun starDirected(n: Int): Graph<Int> {
+fun starDirected(n: Int, type: IntroWindowVM.GraphKeyType): Graph<*> = when (type) {
+    IntroWindowVM.GraphKeyType.INT -> starDirectedInt(n)
+    IntroWindowVM.GraphKeyType.STRING -> starDirectedString(n)
+    IntroWindowVM.GraphKeyType.FLOAT -> starDirectedFloat(n)
+    IntroWindowVM.GraphKeyType.CHAR -> starDirectedChar(n)
+}
+
+private fun starDirectedInt(n: Int): Graph<Int> {
     val graph = Graph<Int>()
-
     graph.addNode(0)
-
     for (i in 1..n) {
         graph.addNode(i)
         graph.addVertice(0, i)
     }
-
     return graph
 }
 
-fun starUndirected(n: Int): Graph<Int> {
+private fun starDirectedString(n: Int): Graph<String> {
+    val graph = Graph<String>()
+    val nodeNames = generateStringNodeNames(n + 1)
+    graph.addNode(nodeNames[0])
+    for (i in 1..n) {
+        graph.addNode(nodeNames[i])
+        graph.addVertice(nodeNames[0], nodeNames[i])
+    }
+    return graph
+}
+
+private fun starDirectedFloat(n: Int): Graph<Float> {
+    val graph = Graph<Float>()
+    graph.addNode(0f)
+    for (i in 1..n) {
+        graph.addNode(i.toFloat())
+        graph.addVertice(0f, i.toFloat())
+    }
+    return graph
+}
+
+private fun starDirectedChar(n: Int): Graph<Char> {
+    val graph = Graph<Char>()
+    graph.addNode('A')
+    for (i in 1..n) {
+        graph.addNode('A' + i)
+        graph.addVertice('A', 'A' + i)
+    }
+    return graph
+}
+
+fun starUndirected(n: Int, type: IntroWindowVM.GraphKeyType): Graph<*> = when (type) {
+    IntroWindowVM.GraphKeyType.INT -> starUndirectedInt(n)
+    IntroWindowVM.GraphKeyType.STRING -> starUndirectedString(n)
+    IntroWindowVM.GraphKeyType.FLOAT -> starUndirectedFloat(n)
+    IntroWindowVM.GraphKeyType.CHAR -> starUndirectedChar(n)
+}
+
+private fun starUndirectedInt(n: Int): Graph<Int> {
     val graph = Graph<Int>()
-
     graph.addNode(0)
-
     for (i in 1..n) {
         graph.addNode(i)
         graph.addVertice(0, i)
         graph.addVertice(i, 0)
+    }
+    return graph
+}
+
+private fun starUndirectedString(n: Int): Graph<String> {
+    val graph = Graph<String>()
+    val nodeNames = generateStringNodeNames(n + 1)
+    graph.addNode(nodeNames[0])
+    for (i in 1..n) {
+        graph.addNode(nodeNames[i])
+        graph.addVertice(nodeNames[0], nodeNames[i])
+        graph.addVertice(nodeNames[i], nodeNames[0])
+    }
+    return graph
+}
+
+private fun starUndirectedFloat(n: Int): Graph<Float> {
+    val graph = Graph<Float>()
+    graph.addNode(0f)
+    for (i in 1..n) {
+        graph.addNode(i.toFloat())
+        graph.addVertice(0f, i.toFloat())
+        graph.addVertice(i.toFloat(), 0f)
+    }
+    return graph
+}
+
+private fun starUndirectedChar(n: Int): Graph<Char> {
+    val graph = Graph<Char>()
+    graph.addNode('A')
+    for (i in 1..n) {
+        graph.addNode('A' + i)
+        graph.addVertice('A', 'A' + i)
+        graph.addVertice('A' + i, 'A')
     }
     return graph
 }
